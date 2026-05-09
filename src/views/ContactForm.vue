17934a23b3ecf23004dcbd85ceedb7cd6eb6157b
kotnikd3 <denis.kotnik@gmail.com>
2026-05-09 17:44:40 +0200

    feat: change EmailJS account credentials

--- src/views/ContactForm.vue
+++ src/views/ContactForm.vue
@@ -88,7 +88,7 @@
                 this.isLoading = true;
                 
                 // service key, template key, form data, {public key}
-                emailjs.send('service_s6u0w2w', 'template_pxuq39a', this.form, {publicKey: 'V62KdOFyQ_XL6WTtd'})
+                emailjs.send('service_nuqxxgt', 'template_6bsct6p', this.form, {publicKey: 'T1xi-L6KPaAZxttmV'})
                 .then(
                     () => {
                         console.log('Email sent!');
